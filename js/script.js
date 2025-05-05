3a34779cf165d5c90fc9f13049fc023badee9ba9
Simon-Ghost-Riley-Tf141 <dharunsrinivasan2004@gmail.com>
2025-05-06 00:23:57 +0530

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-var typed = new Typed(".type",{
-    strings: ['WEB DEVELOPING .','DATA SCIENCE ','PHOTO &amp; VIDEO EDITING.'],
-    typeSpeed: 80,
-    backSpeed: 80,
-    backDelay: 10,
-    loop: true
-})  
-
