1cfa50d7fceb8afaf00cdd5d7573e7e3147f05f3
Simon-Ghost-Riley-Tf141 <dharunsrinivasan2004@gmail.com>
2025-05-06 00:26:35 +0530

    incomplete portfolio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed(".type",{
+    strings: ['WEB DEVELOPING .','DATA SCIENCE ','PHOTO &amp; VIDEO EDITING.'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 10,
+    loop: true
+})  
+
