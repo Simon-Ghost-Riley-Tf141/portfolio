58cc39b75008cb5a1535548b4dfaf4428a0f7fb8
Simon-Ghost-Riley-Tf141 <dharunsrinivasan2004@gmail.com>
2025-05-17 19:03:23 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".type",{
-    strings: ['WEB DEVELOPING .','DATA SCIENCE ','PHOTO &amp; VIDEO EDITING.','STUDENT'],
+    strings: ['WEB DEVELOPER .','DATA SCIENTEST ','PHOTO &amp; VIDEO EDITOR.','STUDENT'],
     typeSpeed: 80,
     backSpeed: 80,
     backDelay: 10,
